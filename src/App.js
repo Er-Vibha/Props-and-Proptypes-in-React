aaf7a4244c9c88a96f764123a0a83be595f33fba
Vibha <65108188+Er-Vibha@users.noreply.github.com>
2024-02-20 15:49:56 +0530

    Props and Proptypes

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import { Navbar } from './Components/Navbar';
+
+function App() {
+  return (
+    <>
+    <Navbar title='TextUtils' about='about text'/>
+    </>
+  );
+}
+export default App;
